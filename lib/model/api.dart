85f7e72f3a1447d76787ef359ea97b066f4b7f7c
Mohammad <18.mamali@gmail.com>
2022-01-20 16:31:36 +0330

    add the api model

---
+++ lib/model/api.dart
@@ -0,0 +1,12 @@
+class Key {
+  static const apiKey = 'YOUR_API_KEY';
+}
+
+class Url {
+  static const baseUrl = 'https://api.themoviedb.org/3';
+  static const imageUrl = 'https://image.tmdb.org/t/p';
+}
+
+class General {
+  static const lang = 'en';
+}
